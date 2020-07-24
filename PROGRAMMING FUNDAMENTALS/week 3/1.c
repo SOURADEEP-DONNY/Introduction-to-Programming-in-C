91bd423d47ac41f15938f31746cbeadfe916a0ec
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-24 18:50:53 +0530

    Add files via upload

---
+++ PROGRAMMING FUNDAMENTALS/week 3/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	char letter='S';
+	int negNumber=-10;
+	unsigned int age =33;
+	printf("My name begins with %c\n",letter);
+	printf("Look I'm negative --> %d\n",negNumber);
+	printf("I am %d years old\n",age);
+	printf("\t in octal(base 8) = %o\n",age);
+	printf("\t in hex(base 16) = %x\n",age);
+}
